9c4769657b0a0841d13fc5f05f61ff695355ff37
Mikey Levine <mrlevine@gmail.com>
2011-08-21 19:30:11 -0700

    Fix up a silly bug and fix up the return value of the method. Getting funny values for the alpha (253, not 255).

--- static/img.js
+++ static/img.js
@@ -1,3 +1,22 @@
+function ByteReader(data) {
+  this.data = data;
+  this.index = 0;
+}
+
+ByteReader.prototype.readByte = function(opt_num) {
+  var num = opt_num || 1;
+  if (this.index + num > this.data.length) {
+    return -1;
+  }
+  var read = 0;
+  var mult = 1;
+  for (; num > 0; --num) {
+    read += mult * (this.data.charCodeAt(this.index++) & 0xff);
+    mult *= 255;
+  }
+  return read;
+};
+
 
 function loadTga(path, done) {
   $.ajax({
@@ -34,44 +53,35 @@ TgaImage.prototype.parse = function() {
   // im dp: pixel depth
   // im de: alpha info
   //
-  // Bytes [19, o1=19 + id): Image data (ignore)
+  // Bytes [19, o1=19 + id): Image info data (ignore)
   // Bytes [o1, o2=o1 + ceil(cm ln * cm sz / 8)): Color map (ignore)
   // Bytes [o2, o2 + ceil(im w * im h * im dp / 8)): Image data.
-  var self = this;
-  var index = 0;
-  function readByte(opt_num) {
-    var num = opt_num || 1;
-    if (index + num > self.data_.length) {
-      return -1;
-    }
-    var read = 0;
-    var mult = 1;
-    for (; num > 0; --num) {
-      read += mult * (self.data_.charCodeAt(index++) & 0xff);
-      mult *= 255;
-    }
-    return read;
-  }
-  var idLen = readByte();
-  var colorMapType = readByte();
-  var imageType = readByte();
+  var reader = new ByteReader(this.data_);
+  var idLen = reader.readByte();
+  var colorMapType = reader.readByte();
+  var imageType = reader.readByte();
 
   // Ignore all the color map (assume we don't have one for now).
-  readByte(5);
+  reader.readByte(5);
   // Ignore the x/y offset.
-  readByte(4);
-  this.width = readByte(2);
-  this.height = readByte(2);
+  reader.readByte(4);
+  this.width = reader.readByte(2);
+  this.height = reader.readByte(2);
   // Ignore the pixel depth and the alpha info.
-  readByte(2);
-  this.pixels = this.data_.substring(index);
+  reader.readByte(2);
+  this.pixels = this.data_.substring(reader.index);
 };
 
 TgaImage.prototype.pixelAt = function(x, y) {
   x = Math.min(this.width - 1, Math.max(0, x));
   y = Math.min(this.height - 1, Math.max(0, y));
   var index = x + (this.height - y - 1) * this.width;
-  return this.pixels.substring(index, index + 4);
+  var reader = new ByteReader(this.pixels.substring(4 * index, 4 * index + 4));
+  var r = reader.readByte();
+  var g = reader.readByte();
+  var b = reader.readByte();
+  var a = reader.readByte();
+  return { r: r, g: g, b: b, a: a };
 };
 
 function parseTga(data, done) {
